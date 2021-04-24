64e0ebbce32a090a3dea84c75f26999afad1467b
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-04-24 18:55:02 +0400

    Solved) Level #3: read instances of dynamically defined classes on all levels

--- json_db.rb
+++ json_db.rb
@@ -2,6 +2,7 @@ require 'json'
 require_relative './mixture'
 
 class JsonDb
+  include DefineCustomClass
   
   def initialize(json_filename)
     @json_filename = json_filename
@@ -20,13 +21,14 @@ class JsonDb
   
   private
 
-  def method_missing(method_name, *args)
-      method_name = method_name.to_s
-      if method_name.include? '='
-        data[method_name.sub('=', '')] = args.first
-      else
-        data[method_name]
-      end
+  def method_missing(method_name, *args, &block)
+    method_name = method_name.to_s
+    if method_name.include? '='
+      data[method_name.sub('=', '')] = args.first
+    else
+      create_property_named_class(method_name, data[method_name])
+      data[method_name]
+    end
   end
   
 end
--- mixture.rb
+++ mixture.rb
@@ -1,14 +1,41 @@
+require 'active_support/inflector'
+
+module DefineCustomClass
+
+  def dynamically_create_class(class_name)
+    return Object.const_get(class_name) if Object.const_defined?(class_name)
+
+    Object.const_set(class_name, Class.new)
+  end
+
+  def create_property_named_class(class_prop_value, data_obj)
+    class_prop_value = class_prop_value.singularize.capitalize
+    return unless data_obj.is_a?(Array)
+
+    data_obj.each { |obj| obj['class'] = class_prop_value }
+  end
+
+end
+
 class Hash
+  include DefineCustomClass
+
+  undef_method :class
 
   private
 
   def method_missing(method_name, *args)
     method_name = method_name.to_s
+    if method_name == 'class'
+      dynamically_create_class(self['class'])
+    else
       if method_name.include? '='
         self[method_name.sub('=', '')] = args.first
       else
+        create_property_named_class(method_name, self[method_name])
         self[method_name]
       end
+    end
   end
 
 end
@@ -20,5 +47,5 @@ class NilClass
   def method_missing(method_name, *args)
     self
   end
-  
+
 end
